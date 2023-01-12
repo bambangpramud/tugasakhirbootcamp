5cebc818d5dbd4d22487110acaaec506619b3d94
Bambang <bambangpramudhitab@gmail.com>
2023-01-12 09:09:04 +0700

    uhuy

--- src/main/java/com/bcafinance/repos/AccountRepo.java
+++ src/main/java/com/bcafinance/repos/AccountRepo.java
@@ -3,7 +3,11 @@ package com.bcafinance.repos;
 import com.bcafinance.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
 public interface AccountRepo extends JpaRepository<Account,Long> {
 
 
+    List<Account> searchAccountByIsPriority( boolean priority);
 }
